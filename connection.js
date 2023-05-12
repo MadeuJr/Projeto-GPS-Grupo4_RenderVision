0d177a641c81155a200ef7b5890b08866b89a1db
Amadeu Andrade Jr <88147833+MadeuJr@users.noreply.github.com>
2023-05-11 22:03:33 -0300

    Atualização com TODOs para se organizar

--- connection.js
+++ connection.js
@@ -2,16 +2,17 @@ const express = require("express");
 const Clientes = require("./backend/routers/cliente.js");
 const Servico = require("./backend/routers/servico.js");
 const Home = require("./backend/routers/home.js");
+const Login = require("./backend/routers/login.js");
 const database = require("./backend/repository/models/sequelizeInit.js");
 const app = express();
-const port = 3000;
+const port = 3001;
 const path = require("path");
 
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(Clientes, Servico, Home);
+app.use(Clientes, Servico, Home, Login);
 
 app.set("views", '/frontend');
 app.set("view engine", "ejs");
