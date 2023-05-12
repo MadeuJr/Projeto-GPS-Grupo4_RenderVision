ba8cde0462e514a8d10c9ceea03d18702e011143
Amadeu Andrade Jr <88147833+MadeuJr@users.noreply.github.com>
2023-05-12 10:44:33 -0300

    Commit com atualização de TODOs e preparação do site

--- connection.js
+++ connection.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const Clientes = require("./backend/routers/cliente.js");
-const Servico = require("./backend/routers/servico.js");
-const Home = require("./backend/routers/home.js");
-const Login = require("./backend/routers/login.js");
-const database = require("./backend/repository/models/sequelizeInit.js");
+const Clientes = require("./src/routers/cliente.js");
+const Servico = require("./src/routers/servico.js");
+const Home = require("./src/routers/home.js");
+const Login = require("./src/routers/login.js");
+const database = require("./src/repository/models/sequelizeInit.js");
 const app = express();
 const port = 3001;
 const path = require("path");
@@ -14,10 +14,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(Clientes, Servico, Home, Login);
 
-app.set("views", '/frontend');
+
 app.set("view engine", "ejs");
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}!`);
-    console.log("Access: http://localhost:3000");
+    console.log(`Access: http://localhost:${port}`);
 });
